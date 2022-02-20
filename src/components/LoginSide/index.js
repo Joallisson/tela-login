edaaf85762790c33c12820cb89c83684bf499f79
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-02-20 00:04:51 -0300

    Estilizando

--- src/components/LoginSide/index.js
+++ src/components/LoginSide/index.js
@@ -1,10 +1,45 @@
 import React from "react";
 import * as Styles from "./style"
 
+//ícones
+import arrow from "../../Assets/arrow.png"
+
 function LoginSide(){
     return(
         <Styles.Content>
-            <h1>Olá Mundo</h1>
+            <Styles.LoginContent>
+                <Styles.AlignBox>
+
+                    <Styles.TitleContent>
+                        <div>
+                            <img src={arrow}/>
+                            <h2>Faça seu Login</h2>
+                        </div>
+                        <p>Entre com suas informações de cadastro</p>
+                    </Styles.TitleContent>
+                    
+                    <Styles.EmailContent>
+                        <span>E-mail</span>
+                        <input type="email"/>
+                    </Styles.EmailContent>
+
+                    <Styles.PasswordContent>
+                        <span>Senha</span>
+                        <input type="password"/>
+                    </Styles.PasswordContent>
+
+                    <Styles.RememberPasswordContent>
+                        <div>
+                            <input type="checkbox"/> <p>Lembre-me</p>
+                        </div>
+                        <span>Esqueci minha senha</span>
+                    </Styles.RememberPasswordContent>
+
+                    <button>Entrar</button>
+                    <span>Não tem uma conta? <p>Registre-se</p></span>
+                    
+                </Styles.AlignBox>
+            </Styles.LoginContent>
         </Styles.Content>
         
     )
--- src/components/LoginSide/style.js
+++ src/components/LoginSide/style.js
@@ -1,10 +1,84 @@
 import styled from 'styled-components'
 
 export const Content = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const LoginContent = styled.div`
     background: #23221e;
     color: #fff;
-    width: 40%;
-    height: 60%;
-    margin-top: 10%;
-    margin-left: 10%;
+    width: 500px;
+    height: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const AlignBox = styled.div`
+    margin-top: 20px;
+    
+`
+
+export const TitleContent = styled.div`
+    color: #7f8285;
+    
+    div{
+        display: flex;
+
+
+        img{
+            width: 20px;
+            height: 20px;
+            margin-top: 5px;
+        }
+
+        h2{
+            margin-top: 0px;
+            margin-left: 5px;
+            font-weight: bold;
+        }
+    }
+
+    p{
+        margin-top: -10px;
+        font-weight: bold;
+    }
+`
+
+export const EmailContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 30px;
+    color: #7f8285;
+
+    input{
+        margin-top: 5px;
+        padding: 12px;
+        background: #23221e;
+        border: 5px solid #f0cd76;
+        border-radius: 10px;
+    }
+`
+
+export const PasswordContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 30px;
+    color: #7f8285;
+
+    input{
+        margin-top: 5px;
+        padding: 12px;
+        background: #23221e;
+        border: 5px solid #f0cd76;
+        border-radius: 10px;
+    }
+`
+
+export const RememberPasswordContent = styled.div`
+
 `
